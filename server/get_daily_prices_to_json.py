132a5d6b98f937c3bb5f89ff4a9bdc8f8eb42f0c
pq <pietu@kissa.fu>
2024-04-09 12:56:31 +0300

    two days in json

--- server/get_daily_prices_to_json.py
+++ server/get_daily_prices_to_json.py
@@ -1,42 +1,62 @@
-
+# tekee jsonin jossa rakenne [ {{date:""}{tunnnit:[0,1,2,3]}}, toinen mokoma]
+# lista  json objekteja joilla pvm ja 4  hina arvoa joka tunnilta
+#palauttaa nykyisen ja  huomisen hinnat
 import requests
 import datetime
 import json
 FILE_PATH = ""
 
-url  = "https://www.sahkohinta-api.fi/api/v1/halpa?"
-
-query  = f"tunnit=24&tulos=sarja&aikaraja=2024-03-29" 
-
-date = datetime.date.today() + datetime.timedelta(days = 1)
-query  = f"tunnit=24&tulos=sarja&aikaraja={date}"
 
 class DayPrices:
     tunnit= {key: None for key in range(24)}
     date_string = ""
 
-    def __init__(self, j_data):
-        date = (datetime.date.today() + datetime.timedelta(days = 1))
+    def __init__(self, j_data,date):
         self.date_string = date.strftime("%Y-%m-%d")
 
         for i in range(len(j_data)):
-            print(float(j_data[i]["hinta"]))
             ## ne on järjestyksessä, ei ees chekata
             self.tunnit[i] = [float(j_data[i]["hinta"]) for k in range(4)] ## koska 15 min hinnoittelu incoming in 2025?
-            print(self.tunnit[i])
     
     def __to_dict__(self):
         return {"date":self.date_string,  "tunnit":self.tunnit}
 
 
-r = requests.get(f"{url}{query}")
-if r.status_code != 200:
-        quit()
-data = r.json()
-dayprices = DayPrices(data)
-print(dayprices.tunnit)
+def getDatFromInternets(date):
+    query = f"tunnit=24&tulos=sarja&aikaraja={date}"
+    r = requests.get(f"{url}{query}")
+    if r.status_code != 200:
+            print(r.status_code)
+            print(r.text)
+            return
+    data = r.json()
+    dayprices = DayPrices(data,date)
 
-x = json.dumps(dayprices.__to_dict__())
+    return dayprices
+    return json.dumps(dayprices.__to_dict__())
+
+
+
+url  = "https://www.sahkohinta-api.fi/api/v1/halpa?" # vaihtaa sit tilale muun kun jos tarvii.
+
+today = datetime.date.today()
+tomorow = datetime.date.today()  + datetime.timedelta(days = 1)
+
+
+prices_today  = getDatFromInternets(today)
+prices_tomorow = getDatFromInternets(tomorow)
+p_list =[]
+for p in [prices_today,prices_tomorow]:
+     if p:
+          p_list.append(p.__to_dict__())
+
+prices = json.dumps(p_list, default=lambda x: x.__dict__)
+print(prices)
+
 file_path = f"{FILE_PATH}dayprices.json"  
-with open(file_path, 'w') as filetowrite:
-    filetowrite.write(x)
+
+if prices: # jos ei palautunut mitään
+    with open(file_path, 'w+') as filetowrite:
+        print("wrote some stuff")
+        filetowrite.write(prices)
+
