79c72e11acf1ff0c5cd79a25c4fb536608565ba1
pq <pietu@kissa.fu>
2024-04-26 11:24:53 +0300

    logiikka toogleamiseen ajan perusteella on

--- main/main.ino
+++ main/main.ino
@@ -7,7 +7,7 @@
 // time stuff
 const char* ntpServer = "fi.pool.ntp.org";
 const int daylight_offset_sec =  3600;
-const int gmt_offset_sec = 3 * 3600;
+const int gmt_offset_sec = 2 * 3600;
 
 const char* ssid     = "kuusipuu";
 const char* password = "koivupuu";
@@ -18,6 +18,12 @@ int btnState = false;
 // runtime stuff
 String json_string;
 struct tm timeinfo; //  järjestelmän aika bro!!!
+int time_slot_arr[24*4]; //  kokoelma time_slotteja hinta järjestyksessä 15 min slotteja 0= 0:0 , 5 = 1:15 jne.
+struct tm t_slot_array_date; // tarkistellaan että toimitaan oikean päivän mukaan
+                            // fallback array?
+bool on_off_arr[24][4];
+int num_on_slots = 16 ; // default time to keep electricity on  15min*4 = 16 slots = 4h
+int used_slots = 0;
 
 void setup()
 {
@@ -93,6 +99,38 @@ void connectWifi(){
         }
     }
 }
+void CreateOnOffArray(){
+// array of 24*4 slots, where slot can be on or off;  
+  // puhdistetaan ensin
+  bool flat_on_off_arr[94];
+  for (int i = 0 ; i<24*4; i++){
+    flat_on_off_arr[i] = false;
+  }
+
+  for ( int i = 0; i < num_on_slots; i++){
+    flat_on_off_arr[time_slot_arr[i]] = true;
+  } // nyt on off sisältää true jokaisessa paikassa jossa haluaan olla päällä.
+
+  // saatanan laiton looppi
+  int flat_index = 0; // yksiuloteisen etsimis indeksi
+  for(int i = 0; i<24; i++){
+    for(int u =0; u <4; u++){
+      on_off_arr[i][u] =flat_on_off_arr[flat_index];
+      flat_index++;
+    }
+  }
+}
+
+void SetOnOff(){
+  int cur_hour = timeinfo.tm_hour;
+  int cur_minute  = timeinfo.tm_min;
+  int vartti = cur_minute/15;
+  if (on_off_arr[cur_hour][vartti] == true){
+    // outputti piälle.
+
+  }
+}
+
 
 
 void loop()
@@ -109,12 +147,6 @@ void loop()
       //Serial.println(json_string);
       delay(1);
       parseJsonAndCalcOnHours(json_string);
-      // Disconnect from WiFi
-      //Serial.println("[WiFi] Disconnecting from WiFi!");
-      // This function will disconnect and turn off the WiFi (NVS WiFi data is kept)
-      //if(WiFi.disconnect(true, false)){
-      //  Serial.println("[WiFi] Disconnected from WiFi!");
-      //}
       
     }    
 }
@@ -157,8 +189,7 @@ void GetTimeFromNtp(){
   struct tm timeinfo;
 }
 
-void printLocalTime()
-{
+void printLocalTime(){
 
   if(!getLocalTime(&timeinfo)){
     Serial.println("No time available (yet)");
@@ -172,6 +203,9 @@ void printLocalTime()
 
 
 bool parseJsonAndCalcOnHours(String input){
+  // returns true if ok and false if failed to parse or current date not pressent in the json
+  // end result will end up in global timeslotarr
+
   JsonDocument doc;
   DeserializationError error = deserializeJson(doc, input);
   // vartti hinnoittelut tänne sisään eli array varttikohtaisia hintoja.
@@ -183,22 +217,16 @@ bool parseJsonAndCalcOnHours(String input){
     Serial.println(error.c_str());
     return false;
   }
+
   // ei virhettä
   int size = doc.size();
-  //Serial.print("docin koko: "); Serial.println(size);
-
 
   bool found = false;
   for ( int i = 0; i< size; i++){
-    //JsonObject ob = doc[i].to<JsonObject>(); /// tää tekee jotain in place tyyppisesti bro!
-    //Serial.println(i);
     const char* d = doc[i]["date"];
-    Serial.print("i looppi : "); Serial.println(i);
     struct tm parsed_date;
     time_t now;
-    //struct tm *cur_date  = $timeinfo;
-    //localtime_r(&now,&timeinfo);
-    
+
     strptime(d, "%Y-%m-%d", &parsed_date);
     Serial.println(&timeinfo,  "%A, %B %d %Y %H:%M:%S");// nykyinen
     Serial.println(&parsed_date,  "%A, %B %d %Y %H:%M:%S"); //parsettu
@@ -206,6 +234,7 @@ bool parseJsonAndCalcOnHours(String input){
         parsed_date.tm_mon == timeinfo.tm_mon &&
         parsed_date.tm_mday == timeinfo.tm_mday) {
           found = true;
+          t_slot_array_date = timeinfo; // asetetaan muuttuja joka kertoo minkä päivän tietoa soratussa arrayssa on
           Serial.println("The parsed date is today.");
           // tämän päivähn tedot tauluun kissa
           JsonObject tunnit = doc[i]["tunnit"];
@@ -216,73 +245,64 @@ bool parseJsonAndCalcOnHours(String input){
                 prices_today[h][i] = kv.value()[i];
               }
             }
-            Serial.println(prices_today[0][1]);
+            
           }
 
     else {
           //Serial.println("The parsed date is not today.\n");        
     }
   }
-  if (!found) { return false;}
-  //Serial.println(prices_today[20][0]);
-  SortTimesAsc(prices_today ); // toinen param target taulukko!!
+  if (!found) 
+        { return false;}
+
+  SortTimesAsc(prices_today ); 
+
   return true;
 }
 
 
 
-void SortTimesAsc(float prices[24][4]){ // cee velho korjaa tän pass by reffiksi vielä 
+void SortTimesAsc(float prices[24][4]){
   Serial.println(prices[0][1]);
   // flaten the array do a pseudo map :D
   float value_arr[24*4];
   int flat_i =0;
   for (int i = 0; i < 4; i++){
     for(int u = 0; u < 24; u++){
-      //Serial.println(prices[i][u]);
       value_arr[flat_i] =  prices[i][u];
       flat_i++;
     }
   }
-  // timeslot array
-  int time_slot_arr[24*4];
-  for ( int i = 0 ; i < 24*4; i++){
+  // timeslot array populated with  matching indices
+   for ( int i = 0 ; i < 24*4; i++){
     time_slot_arr[i] = i;
   }
-  Serial.println(value_arr[1]);
-  // sort value arr and do same transforms on timeslot arr
-  
+
   // insertion sort 
-
-  insertionSort(value_arr, time_slot_arr, 94);
-
+    insertionSort(value_arr, time_slot_arr, 94);
+  
 }
 
 /* Function to sort an array using insertion sort*/
-void insertionSort(float arr[], int ind_arr[], int n)
-{
-  Serial.println(arr[1]);
+void insertionSort(float arr[], int timeslot_arr[], int n){ // nää on pass by ref vaikka en täysin käsitäkkään!!
+  // sorts based on first arr  does transorms on both arrays.
+  //Serial.println(arr[1]);
     float key, ind_key;
     int i, j;
   
     for (i = 1; i < n; i++) {
         key = arr[i];
-        ind_key = ind_arr[i];
+        ind_key = timeslot_arr[i];
         j = i - 1;
-
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            ind_arr[j + 1] = ind_arr[j];
+            timeslot_arr[j + 1] = timeslot_arr[j];
             j = j - 1;
         }
         arr[j + 1] = key;
-        ind_arr[j+1] = ind_key;
+        timeslot_arr[j+1] = ind_key;
     }
- 
-
- for (int i = 0; i<94;i++){
-  Serial.println(arr[i]);
-  Serial.println(ind_arr[i]);
- }
 }
+
 
 
