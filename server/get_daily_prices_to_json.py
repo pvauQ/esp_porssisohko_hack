1006c07078be386be353cb5a7fdc46cf687a168c
pq <pietu@kissa.fu>
2024-05-15 09:27:28 +0300

    fiks pricethingy more

--- server/get_daily_prices_to_json.py
+++ server/get_daily_prices_to_json.py
@@ -11,6 +11,8 @@ def parseToDict(data, date):
         
     date_string = date.strftime("%Y-%m-%d") 
     tunnit = {}
+    if not data:
+         return
     for i in range(len(data)):
         tunnit[i] = [float(data[i]["hinta"]) for k in range(4)] ## koska 15 min hinnoittelu incoming in 2025?
     res = {"date":date_string,  "tunnit":tunnit}
@@ -20,7 +22,6 @@ def getDatFromInternets(date):
     query = f"tunnit=24&tulos=sarja&aikaraja={date}"
     r = requests.get(f"{url}{query}")
     print(query)
-    print("kissa")
     if r.status_code != 200:
             print(r.status_code)
             print(r.text)
@@ -33,20 +34,22 @@ url  = "https://www.sahkohinta-api.fi/api/v1/halpa?" # vaihtaa sit tilale muun k
 
 today = datetime.date.today()
 tomorow = datetime.date.today()  + datetime.timedelta(days = 1)
-
 prices_today  = getDatFromInternets(today)
 prices_tomorow = getDatFromInternets(tomorow)
+arr = []
 p1 = parseToDict(prices_today,today)
 p2= parseToDict(prices_tomorow,tomorow)
+if prices_today: arr.append(p1)
+if prices_tomorow: arr.append(p2)
 
 
-prices = json.dumps([p1,p2])
+prices = json.dumps(arr)
 print(prices)
 
 file_path = f"{FILE_PATH}dayprices.json"  
 
 if prices: # jos ei palautunut mitään
     with open(file_path, 'w+') as filetowrite:
-        print("wrote some stuff")
+        
         filetowrite.write(prices)
-
+        print("wrote some stuff")
